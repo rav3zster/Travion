0de229db84d1414bd5faacdb399b702278419f6c
Ravikiran27 <143682277+Ravikiran27@users.noreply.github.com>
2025-09-26 21:40:37 +0530

    map

--- travelai/lib/mapmyindia_config.dart
+++ travelai/lib/mapmyindia_config.dart
@@ -1,6 +1,8 @@
 // MapmyIndia API credentials
 class MapmyIndiaConfig {
   static const String restMapSdkKey = '9ddc663ea2a95a89b26331a1874fd2e2';
-  static const String clientId = '96dHZVzsAuvfO-QEzWWTEGgpRDWpGpFcXxYrsLOICdC9Af-Z0KA4QGz7cJYkYuWLxsvVbKJAf_zPPafqjv84iw==';
-  static const String clientSecret = 'lrFxI-iSEg8fSznZmgJUTj5kluAvq8vaYD6lQAN8zW1D3bjjMRsPGj3Dq3nwQsutPnTJZj-F5E6Zp6uBIbln2_FtRwn6BcCC';
+  static const String clientId =
+      '96dHZVzsAuvfO-QEzWWTEGgpRDWpGpFcXxYrsLOICdC9Af-Z0KA4QGz7cJYkYuWLxsvVbKJAf_zPPafqjv84iw==';
+  static const String clientSecret =
+      'lrFxI-iSEg8fSznZmgJUTj5kluAvq8vaYD6lQAN8zW1D3bjjMRsPGj3Dq3nwQsutPnTJZj-F5E6Zp6uBIbln2_FtRwn6BcCC';
 }
